912d3e3e72019f5188849a14bcc0c5d2322c5bfb
Dion <dionjones132@gmail.com>
2024-12-02 13:19:51 +0000

    jspackage

--- vite.config.js
+++ vite.config.js
@@ -11,12 +11,6 @@ export default defineConfig({
     build: {
         outDir: 'public/build',
         manifest: true,
-        rollupOptions: {
-            input: {
-                app: 'resources/js/app.js',
-                style: 'resources/css/app.css'
-            }
-        }
     },
     resolve: {
         alias: {
