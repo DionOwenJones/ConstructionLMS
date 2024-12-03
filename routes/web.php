46ff5b482bf004e26d38ab6cb2698c8a9240ae7b
Dion <dionjones132@gmail.com>
2024-12-03 13:09:28 +0000

    'test'

--- routes/web.php
+++ routes/web.php
@@ -121,23 +121,12 @@ Route::get('/test-email', function() {
 
 //login + register routes
 Route::middleware('guest')->group(function () {
-    // Authentication Routes
-    Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
-    Route::post('login', [LoginController::class, 'login']);
-    Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-    Route::post('register', [RegisterController::class, 'register']);
-
     // Password Reset Routes
     Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
     Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
     Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
     Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
-    
 });
-
-Route::post('logout', [LoginController::class, 'destroy'])
-    ->name('logout')
-    ->middleware('auth');
 
 // Email Verification Routes
 Route::get('/email/verify', function () {
